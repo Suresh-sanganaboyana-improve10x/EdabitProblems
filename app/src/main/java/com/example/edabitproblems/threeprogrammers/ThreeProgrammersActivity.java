e63b609636beb1086f548ce1be559f6ef5d145a4
Suresh <suresh.sanganaboyana@improve10x.com>
2023-02-09 15:34:30 +0530

    created edabit problems project

---
+++ app/src/main/java/com/example/edabitproblems/threeprogrammers/ThreeProgrammersActivity.java
@@ -0,0 +1,16 @@
+package com.example.edabitproblems.threeprogrammers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.edabitproblems.R;
+
+public class ThreeProgrammersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three_programmers);
+    }
+}
