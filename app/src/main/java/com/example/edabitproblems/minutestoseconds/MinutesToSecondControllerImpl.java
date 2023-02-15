4962c3d0dda41d6a8166c7c61963ecd2ab2055a1
Suresh <suresh.sanganaboyana@improve10x.com>
2023-02-15 10:51:27 +0530

    Implemented Exception concepts in MinutesToSecondControllerImpl.java

--- app/src/main/java/com/example/edabitproblems/minutestoseconds/MinutesToSecondControllerImpl.java
+++ app/src/main/java/com/example/edabitproblems/minutestoseconds/MinutesToSecondControllerImpl.java
@@ -1,11 +1,17 @@
 package com.example.edabitproblems.minutestoseconds;
 
+import android.util.Log;
+
 public class MinutesToSecondControllerImpl implements MinutesToSecondsController{
     @Override
     public int minutesToSeconds(String minutes) {
-        int minutesInInt = Integer.parseInt(minutes);
         int seconds = 0;
-        seconds = minutesInInt*60;
+        try {
+            int minutesInInt = Integer.parseInt(minutes);
+            seconds = minutesInInt * 60;
+        } catch (Exception ex) {
+            Log.e("", ex.getMessage(), ex);
+        }
         return seconds;
     }
 }
